faa3e92e04e8aa5f7bd47edda79fa4b7cb77241a
Olia Litvinova <iamnephelim@gmail.com>
2016-09-20 15:05:40 -0700

    Initial commit

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -7,6 +7,7 @@
     Create a controller using this template
 """
 from system.core.controller import *
+import random
 
 class Welcome(Controller):
     def __init__(self, action):
@@ -19,22 +20,39 @@ class Welcome(Controller):
         self.db = self._app.db
 
         """
-        
-        This is an example of a controller method that will load a view for the client 
+
+        This is an example of a controller method that will load a view for the client
 
         """
-   
+
     def index(self):
-        """
-        A loaded model is accessible through the models attribute 
-        self.models['WelcomeModel'].get_users()
-        
-        self.models['WelcomeModel'].add_message()
-        # messages = self.models['WelcomeModel'].grab_messages()
-        # user = self.models['WelcomeModel'].get_user()
-        # to pass information on to a view it's the same as it was with Flask
-        
-        # return self.load_view('index.html', messages=messages, user=user)
-        """
+        if 'gold' not in session:
+            session['gold'] = 0
+        if 'log' not in session:
+            session['log'] = ''
+        session['farm'] = random.randrange(10, 21)
+        session['cave'] = random.randrange(5, 11)
+        session['house'] = random.randrange(2, 6)
+        session['casino'] = random.randrange(-50, 51)
         return self.load_view('index.html')
+    def process(self):
+        session['title'] = ""
+        if request.form['building'] == 'farm':
+            session['gold'] += session['farm']
+            message = "<div> You clicked farm and you got " + " " + str(session['farm']) + "</div><br>"
+        elif request.form['building'] == 'cave':
+            session['gold'] += session['cave']
+            message = "You clicked cave and you got" + " " +  str(session['cave'])
+        elif request.form['building'] == 'house':
+            session['gold'] += session['house']
+            message = "You clicked house and you got" + " " + str(session['house'])
+        elif request.form['building'] == 'casino':
+            session['gold'] += session['casino']
+            message = "You clicked casino and you got" + " " +  str(session['casino'])
 
+        session['log'] += message
+        return redirect('/')
+    def reset(self):
+        session['gold'] = 0
+        session['log'] = ''
+        return redirect('/')
